a3514257cd05adbfba717dbf352ce93c7848bc76
ZhaiXiaoQing <zhaixq@zonekey.com.cn>
2017-12-20 19:14:54 +0800

    remove code

--- globalobject.js
+++ globalobject.js
@@ -6,22 +6,14 @@ function pritnHelloWorld() {
     console.log('hellowolrd');
 }
 var t = setTimeout(pritnHelloWorld, 3000);
-
 //测试清除定时器
 clearTimeout(t);
 
-
 //测试setInterval
 var t1=setInterval(pritnHelloWorld,2000);
-
-//
 clearInterval(t1);
 
 
-
-
-
 process.on('exit',function(){
-
     console.log('程序退出');
 })
